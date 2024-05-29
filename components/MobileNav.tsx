f8cc386545921650babfec0dc1d396224d2ca446
weiiwu <master_wu51@hotmail.com>
2024-05-29 16:45:07 +1000

    Implement updates

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -14,6 +14,7 @@ import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import Footer from './Footer';
 
 const MobileNav = ({ user }: MobileNavProps) => {
     const pathname = usePathname();
@@ -64,7 +65,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                                 USER
                             </nav>
                         </SheetClose>
-                        FOOTER
+                        <Footer user={user} type="mobile" />
                     </div>
                 </SheetContent>
             </Sheet>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import { sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils';
 import { usePathname } from 'next/navigation';
+import Footer from './Footer';
 
 const SideBar = ({ user }: SiderbarProps) => {
 
@@ -42,7 +43,7 @@ const SideBar = ({ user }: SiderbarProps) => {
                 USER
             </nav>
 
-            FOOTER
+            <Footer user={user} />
         </section>
     )
 }
